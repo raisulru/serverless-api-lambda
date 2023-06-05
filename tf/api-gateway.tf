23f312f7ea6280a29797f4d514293724b9067aeb
raisulru <raisulru10@gmail.com>
2023-06-06 00:37:53 +0600

    Add lambda function

--- tf/api-gateway.tf
+++ tf/api-gateway.tf
@@ -1,36 +1,46 @@
-resource "aws_api_gateway_rest_api" "data_server" {
-  body = jsonencode({
-    openapi = "3.0.1"
-    info = {
-      title   = "data-server"
-      version = "1.0"
-    }
-    paths = {
-      "/" = {
-        get = {
-          x-amazon-apigateway-integration = {
-            httpMethod           = "GET"
-            payloadFormatVersion = "1.0"
-            type                 = "HTTP_PROXY"
-            uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
-          }
-        }
-      }
-    }
-  })
-
-  name = "${var.prefix}-data-server"
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
+resource "aws_api_gateway_rest_api" "example" {
+  name = "example"
 }
 
-resource "aws_api_gateway_deployment" "data_server_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.data_server.id
+resource "aws_api_gateway_resource" "example" {
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "example"
+  rest_api_id = aws_api_gateway_rest_api.example.id
+}
+
+resource "aws_api_gateway_method" "example" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.example.id
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+}
+
+resource "aws_api_gateway_integration" "example" {
+  http_method = aws_api_gateway_method.example.http_method
+  resource_id = aws_api_gateway_resource.example.id
+  rest_api_id = aws_api_gateway_rest_api.example.id
+
+#   integration_http_method = "POST"
+  type                    = "MOCK"
+#   uri                     = "${aws_lambda_function.example.invoke_arn}"
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
 
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.data_server.body))
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.example.id,
+      aws_api_gateway_method.example.id,
+      aws_api_gateway_integration.example.id,
+    ]))
   }
 
   lifecycle {
@@ -38,8 +48,8 @@ resource "aws_api_gateway_deployment" "data_server_deployment" {
   }
 }
 
-resource "aws_api_gateway_stage" "dev_stage" {
-  deployment_id = aws_api_gateway_deployment.data_server_deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.data_server.id
-  stage_name    = "${var.prefix}-dev"
+resource "aws_api_gateway_stage" "example" {
+  deployment_id = aws_api_gateway_deployment.example.id
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  stage_name    = "example"
 }
---
+++ tf/lambda.tf
@@ -0,0 +1,42 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "${path.module}/lambda.js"
+  output_path = "${path.module}/lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "${path.module}/lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda.handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "nodejs16.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
