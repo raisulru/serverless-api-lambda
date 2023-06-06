db8bd96249156c0aeed59c326d68ea7dea87cc8d
raisulru <raisulru10@gmail.com>
2023-06-06 22:54:33 +0600

    Integrate lambda to api gateway

--- tf/api-gateway.tf
+++ tf/api-gateway.tf
@@ -8,40 +8,64 @@ resource "aws_api_gateway_resource" "example" {
   rest_api_id = aws_api_gateway_rest_api.example.id
 }
 
-resource "aws_api_gateway_method" "example" {
+resource "aws_api_gateway_method" "proxy" {
   authorization = "NONE"
   http_method   = "GET"
   resource_id   = aws_api_gateway_resource.example.id
   rest_api_id   = aws_api_gateway_rest_api.example.id
 }
 
-resource "aws_api_gateway_integration" "example" {
-  http_method = aws_api_gateway_method.example.http_method
+resource "aws_api_gateway_integration" "lambda" {
+  http_method = aws_api_gateway_method.proxy.http_method
   resource_id = aws_api_gateway_resource.example.id
   rest_api_id = aws_api_gateway_rest_api.example.id
 
-#   integration_http_method = "POST"
-  type                    = "MOCK"
-#   uri                     = "${aws_lambda_function.example.invoke_arn}"
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.test_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_rest_api.example.root_resource_id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  resource_id = aws_api_gateway_method.proxy_root.resource_id
+  http_method = aws_api_gateway_method.proxy_root.http_method
+
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.test_lambda.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "example" {
   rest_api_id = aws_api_gateway_rest_api.example.id
 
-  triggers = {
-    # NOTE: The configuration below will satisfy ordering considerations,
-    #       but not pick up all future REST API changes. More advanced patterns
-    #       are possible, such as using the filesha1() function against the
-    #       Terraform configuration file(s) or removing the .id references to
-    #       calculate a hash against whole resources. Be aware that using whole
-    #       resources will show a difference after the initial implementation.
-    #       It will stabilize to only change when resources change afterwards.
-    redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.example.id,
-      aws_api_gateway_method.example.id,
-      aws_api_gateway_integration.example.id,
-    ]))
-  }
+  depends_on = [
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root
+  ]
+
+  #   triggers = {
+  #     # NOTE: The configuration below will satisfy ordering considerations,
+  #     #       but not pick up all future REST API changes. More advanced patterns
+  #     #       are possible, such as using the filesha1() function against the
+  #     #       Terraform configuration file(s) or removing the .id references to
+  #     #       calculate a hash against whole resources. Be aware that using whole
+  #     #       resources will show a difference after the initial implementation.
+  #     #       It will stabilize to only change when resources change afterwards.
+  #     redeployment = sha1(jsonencode([
+  #       aws_api_gateway_resource.example.id,
+  #       aws_api_gateway_method.proxy.id,
+  #       aws_api_gateway_integration.lambda.id,
+  #       aws_api_gateway_method.proxy.id,
+  #       aws_api_gateway_integration.lambda.id
+  #     ]))
+  #   }
 
   lifecycle {
     create_before_destroy = true
--- tf/lambda.tf
+++ tf/lambda.tf
@@ -40,3 +40,11 @@ resource "aws_lambda_function" "test_lambda" {
     }
   }
 }
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+}
